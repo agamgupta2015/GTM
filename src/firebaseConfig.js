188e27e6d695e8867c3833676a718cc69474ac71
Agam Sharan Gupta <75520558+agamgupta2015@users.noreply.github.com>
2024-07-12 14:36:41 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBIQ7z5nZyiJnyEEyz8yI5h-AiLXCORx0",
-  authDomain: "assign-1dc2c.firebaseapp.com",
-  projectId: "assign-1dc2c",
-  storageBucket: "assign-1dc2c.appspot.com",
-  messagingSenderId: "987553341050",
-  appId: "1:987553341050:web:32f7be73f10362508c25c3",
-  measurementId: "G-2NRW3J3679",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
